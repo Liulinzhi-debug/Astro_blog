91f4e13e69f7d610476c3f358cfdd7d6f939c7f5
Liulinzhi-debug <491383674@qq.com>
2026-01-06 13:27:40 +0800

    update EDIT

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import remarkMath from 'remark-math'; // 新增
+import rehypeKatex from 'rehype-katex'; // 新增
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
+	markdown: {
+		remarkPlugins: [remarkMath], // 启用 Math
+		rehypePlugins: [rehypeKatex], // 启用 KaTeX
+	},
 	integrations: [mdx(), sitemap()],
 });
